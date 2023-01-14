ccea9f8bec433ce95ae050fdd29a22b400b35768
Ewelina Nowak <ewelinanowak@Ewelinas-MacBook-Pro.local>
2023-01-14 21:49:14 +0100

    finished Sonny's tutorial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "links.papareact.com",
+      "v.wpimg.pl",
+      "scontent.fpoz4-1.fna.fbcdn.net",
+      "platform-lookaside.fbsbx.com",
+    ],
+  },
+  experimental: {
+    appDir: true,
+  },
+};
